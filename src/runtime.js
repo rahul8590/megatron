e074d3092f1d92b5fb0dd5277bab9f65ccacab15
Rahul <rahul@g3ck0.(none)>
2013-10-15 19:05:54 -0400

    Adding node detection in detect_browser

--- src/runtime.js
+++ src/runtime.js
@@ -1,36 +1,44 @@
 function detect_browser() {
-    var nAgt = navigator.userAgent;
-    var browserName  = navigator.appName;
-    var nameOffset,verOffset,ix;
+    
+    if (type window == 'undefined') {
+      browserName = 'node';
+    }
 
-    // In Opera, the true version is after "Opera" or after "Version"
-    if ((verOffset=nAgt.indexOf("Opera"))!=-1) {
-     browserName = "Opera";
+    else {
+      var nAgt = navigator.userAgent;
+      var browserName  = navigator.appName;
+      var nameOffset,verOffset,ix;
+
+      // In Opera, the true version is after "Opera" or after "Version"
+      if ((verOffset=nAgt.indexOf("Opera"))!=-1) {
+       browserName = "Opera";
+      }
+      // In MSIE, the true version is after "MSIE" in userAgent
+      else if ((verOffset=nAgt.indexOf("MSIE"))!=-1) {
+       }
+      // In Chrome, the true version is after "Chrome" 
+      else if ((verOffset=nAgt.indexOf("Chrome"))!=-1) {
+       browserName = "Chrome";
+      }
+      // In Safari, the true version is after "Safari" or after "Version" 
+      else if ((verOffset=nAgt.indexOf("Safari"))!=-1) {
+       browserName = "Safari";
+      }
+      // In Firefox, the true version is after "Firefox" 
+      else if ((verOffset=nAgt.indexOf("Firefox"))!=-1) {
+       browserName = "Firefox";
+      }
+      // In most other browsers, "name/version" is at the end of userAgent 
+      else if ( (nameOffset=nAgt.lastIndexOf(' ')+1) < 
+                (verOffset=nAgt.lastIndexOf('/')) ) 
+      {
+       browserName = nAgt.substring(nameOffset,verOffset);
+       if (browserName.toLowerCase()==browserName.toUpperCase()) {
+        browserName = navigator.appName;
+       }
+      }  
     }
-    // In MSIE, the true version is after "MSIE" in userAgent
-    else if ((verOffset=nAgt.indexOf("MSIE"))!=-1) {
-     }
-    // In Chrome, the true version is after "Chrome" 
-    else if ((verOffset=nAgt.indexOf("Chrome"))!=-1) {
-     browserName = "Chrome";
-    }
-    // In Safari, the true version is after "Safari" or after "Version" 
-    else if ((verOffset=nAgt.indexOf("Safari"))!=-1) {
-     browserName = "Safari";
-    }
-    // In Firefox, the true version is after "Firefox" 
-    else if ((verOffset=nAgt.indexOf("Firefox"))!=-1) {
-     browserName = "Firefox";
-    }
-    // In most other browsers, "name/version" is at the end of userAgent 
-    else if ( (nameOffset=nAgt.lastIndexOf(' ')+1) < 
-              (verOffset=nAgt.lastIndexOf('/')) ) 
-    {
-     browserName = nAgt.substring(nameOffset,verOffset);
-     if (browserName.toLowerCase()==browserName.toUpperCase()) {
-      browserName = navigator.appName;
-     }
-    }
+    
     return browserName;
 }
 

c0c01a0762dc002f23ce6adffa5fa5a1f36f9266
Ted Smith <tedks@cs.umass.edu>
2013-10-15 21:09:31 -0400

    back to original failing test after being used as a scratchpad

--- test/calleecallexpression.js
+++ test/calleecallexpression.js
@@ -1,15 +1,1 @@
-function bar() { 
-    console.log("bar");
-    return 10;
-}
-
-function foo() { 
-    console.log("foo");
-    return bar;
-}
-var baz = foo();
-baz();
-var foobar = foo();
-foobar();
-
-
+(function () { return (function () { return 1;});})()();

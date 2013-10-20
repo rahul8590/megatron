71e82d3a029802326ac1c93b46d1f4f1b0c05ec2
Rahul <rahul@g3ck0.(none)>
2013-10-20 04:46:09 -0400

    added simple index.js (no need of server framework) and socket streaming

---
+++ index.js
@@ -0,0 +1,74 @@
+var app = require('http').createServer(handler)
+  , io = require('socket.io').listen(app)
+  , fs = require('fs')
+  , url = require('url')
+  , querystring = require('querystring');
+
+app.listen(8590);
+
+function handler (req, res) {
+  
+  var url_parts = url.parse(req.url);
+  console.log(url_parts.path);
+  var path;
+  var post_data = '';
+  
+  if (url_parts.path == '/graph') {
+	path = '/public/graph.html';  
+	}
+
+	if (url_parts.path == '/bootstrap.css') {
+	path = '/public/bootstrap.css';  
+	}
+	
+	if (url_parts.path == '/springy.js') {
+	path = '/public/springy.js';  
+	}
+	
+	if (url_parts.path == '/jumbotron-narrow.css') {
+	path = '/public/jumbotron-narrow.css';  
+	}
+	
+	if (url_parts.path == '/springyui.js') {
+	path = '/public/springyui.js';  
+	}
+	
+	if (url_parts.path == '/') {
+	path = '/public/index.html';  
+	}
+	 
+ 
+	 
+	 fs.readFile(__dirname + path,
+	 function (err,data) {
+	  if(err) {
+		  res.writeHead(500);
+		  return res.end("Error loading");
+		}
+		if(req.method == 'POST') {
+			req.on('data',function(chunk){
+				post_data += chunk.toString();
+			});
+			req.on('end',function() {
+			var decbody = querystring.parse(post_data);
+			console.log("===============");
+			console.log(decbody.code);
+			console.log("===============");
+			});
+		}
+		
+		res.writeHead(200);
+		res.end(data);
+	});
+}
+
+io.sockets.on('connection', function (socket) {
+	// Adding Sample Call object which I will receive from another function
+	// Server will emit graph signal to FE and send the call object.
+	var call = {func_name: 'world'};
+	socket.emit('graph', JSON.stringify(call));
+	//socket.on('ack', function (data) {
+    //console.log(data);
+  });
+
+
